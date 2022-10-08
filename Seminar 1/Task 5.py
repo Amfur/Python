01e04a7e756bda218efcd967ae65182ffeea1703
Ganiev Konstantin <socingin@ya.ru>
2022-10-08 20:22:02 +0400

    fix code S1/T5

--- Seminar 1/Task 5.py
+++ Seminar 1/Task 5.py
@@ -3,5 +3,6 @@
 first_point = [int(input('x1 = ')), int(input('y1 = '))]
 second_point = [int(input('x2 = ')), int(input('y2 = '))]
 print(f'Заданы точки: ({first_point[0]};{first_point[1]}) и ({second_point[0]};{second_point[1]})')
-distance = ((first_point[0] - second_point[0])*(first_point[0] - second_point[0]) + (first_point[1] - second_point[1])*(first_point[1] - second_point[1]))**0.5
+distance = ((first_point[0] - second_point[0])*(first_point[0] - second_point[0])
++ (first_point[1] - second_point[1])*(first_point[1] - second_point[1]))**0.5
 print(f'Расстояние между точками: {distance}')
