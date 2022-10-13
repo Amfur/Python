05059cd593dab81aa2d53ba5df0d8f534188eaa1
Ganiev Konstantin <socingin@ya.ru>
2022-10-14 01:30:07 +0400

    add code S2/T4

--- Seminar 2/Task 4.py
+++ Seminar 2/Task 4.py
@@ -1,1 +1,23 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на введенных пользователем позициях.
+
+N = abs(int(input('Задайте размер списка: ')))
+count = N
+interval = []
+elem = None
+while count != 0:
+    elem = int(input(f'Осталось ввести: {count}\nВведите значение от {-N} до {N}: '))
+    if -N <= elem <= N:
+        interval.append(elem)
+        count -= 1
+print(interval)
+if len(interval) != 0:
+    count = abs(int(input('Введите количество множителей: ')))
+    prod = 1
+    while count != 0 and len(interval) != 0:
+        elem = int(input(f'Осталось ввести: {count}\nВведите позицию элемента: '))
+        if elem in range(len(interval)) and elem >= 0:
+            prod *= interval[elem]
+            count -= 1
+    print(prod)
+else:
+    print('Задан пустой список. Расчёт произведения невозможен')
