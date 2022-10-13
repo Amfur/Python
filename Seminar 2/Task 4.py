c980ad925e4b4d547a7805ec46bb736712947132
Ganiev Konstantin <socingin@ya.ru>
2022-10-14 01:39:51 +0400

    fix S2/T4

--- Seminar 2/Task 4.py
+++ Seminar 2/Task 4.py
@@ -13,11 +13,14 @@ print(interval)
 if len(interval) != 0:
     count = abs(int(input('Введите количество множителей: ')))
     prod = 1
+    indexes = []
     while count != 0 and len(interval) != 0:
         elem = int(input(f'Осталось ввести: {count}\nВведите позицию элемента: '))
         if elem in range(len(interval)) and elem >= 0:
+            indexes.append(elem)
             prod *= interval[elem]
             count -= 1
-    print(prod)
+    print(indexes)
+    print(f'Произведение элементов на введённых позициях: {prod}')
 else:
     print('Задан пустой список. Расчёт произведения невозможен')
