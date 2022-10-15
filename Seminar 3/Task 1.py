9e3207d1a2828874a4595fa352a19a40f3fdae28
Ganiev Konstantin <socingin@ya.ru>
2022-10-15 12:40:47 +0400

    add function in S3/T1

--- Seminar 3/Task 1.py
+++ Seminar 3/Task 1.py
@@ -1,11 +1,17 @@
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 import random
-amouth_nums = abs(int(input('Сколько чисел поместить в список? ')))
-my_list = []
-for i in range(amouth_nums):
-    my_list.append(random.randint(0, 10))
+
+def create_list(elems):
+    elems = abs(int(elems))
+    new_list = []
+    for i in range(elems):
+        new_list.append(random.randint(0, 10))
+    return new_list
+
+my_list = create_list(input('На сколько элементов создать список? '))
 print(my_list)
+
 sum = 0
 odd_elems = []
 for i in range(1,len(my_list),2):
