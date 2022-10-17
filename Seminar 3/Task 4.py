f01ba03617f43e41a425b8276f0475cc9eec9ad4
Ganiev Konstantin <socingin@ya.ru>
2022-10-17 16:35:45 +0400

    add code S3/T4

--- Seminar 3/Task 4.py
+++ Seminar 3/Task 4.py
@@ -4,3 +4,25 @@
 # 3 -> 11
 # 2 -> 10
 
+def dec_to_bin(num):
+    num = int(num)
+    if num < 0:
+        num = abs(num)
+        frac = 0
+        bin = ''
+        while num != 0:
+            frac = num % 2
+            bin = str(frac) + bin
+            num //= 2
+        bin = '-' + bin
+    else:
+        frac = 0
+        bin = ''
+        while num != 0:
+            frac = num % 2
+            bin = str(frac) + bin
+            num //= 2
+    return bin
+
+binary = dec_to_bin(input('Введите число: '))
+print(f'Его двоичная запись: {binary}')
